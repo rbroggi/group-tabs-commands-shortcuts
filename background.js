728ff7995da739e84805906847051c0a2c3c207f
rbroggi <ro_broggi@hotmail.com>
2021-11-27 12:17:01 +0100

    new tab in group working

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
-let color = '#3aa757';
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+chrome.commands.onCommand.addListener(function(command) {
+    console.log("invoked!!")
+    chrome.tabs.query({currentWindow: true, active: true}, function(foo) {
+        var cur = foo[0];
+        chrome.tabs.create({index: cur.index+1}, function(bar) {
+            chrome.tabs.group({tabIds: bar.id, groupId: cur.groupId});
+        });
+    });
 });
