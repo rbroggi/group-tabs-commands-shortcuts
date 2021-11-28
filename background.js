d58582abd350b02d6dee127f946f964ed519e4b7
rbroggi <ro_broggi@hotmail.com>
2021-11-28 19:29:06 +0100

    shortcuts configured

--- background.js
+++ background.js
@@ -1,10 +1,37 @@
 
 chrome.commands.onCommand.addListener(function(command) {
-    console.log("invoked!!")
-    chrome.tabs.query({currentWindow: true, active: true}, function(foo) {
-        var cur = foo[0];
-        chrome.tabs.create({index: cur.index+1}, function(bar) {
-            chrome.tabs.group({tabIds: bar.id, groupId: cur.groupId});
-        });
-    });
+  switch(command) {
+    case "new-tab-in-group":
+      createNewTabInCurrentGroup()
+      break;
+    case "collapse-all-groups":
+      collapseOrExpandAllGroups(true);
+      break;
+    case "expand-all-groups":
+      collapseOrExpandAllGroups(false);
+      break;
+    default:
+      console.log("Not implemented");
+  }
+
 });
+
+
+function createNewTabInCurrentGroup() {
+  chrome.tabs.query({currentWindow: true, active: true}, function(foo) {
+      var cur = foo[0];
+      chrome.tabs.create({index: cur.index+1}, function(bar) {
+          chrome.tabs.group({tabIds: bar.id, groupId: cur.groupId});
+      });
+  });
+}
+
+function collapseOrExpandAllGroups(shouldCollapse) {
+  chrome.tabGroups.query({}, function(groups) {
+      var groupIDs = groups.map(group => group.id);
+      groupIDs.forEach( id => {
+        chrome.tabGroups.update(id, {collapsed: shouldCollapse});
+      });
+  });
+}
+
